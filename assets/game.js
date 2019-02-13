73474eee8c52f60f659774fde5f6520774d27f42
Perry Sham <perrysham@dyn-160-39-163-237.dyn.columbia.edu>
2019-02-13 18:39:33 -0500

    2/13

---
+++ assets/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+// declare variables
+var dealerNum = [];
+var playerNum = [];
+
+
+// initiates random number for dealer & displays value
+
+
+// initiates random number for player cards
+
+
+// on click - displays card values
+
+
+// sums card values
+
+
+// if sum exceeds dealer value, stop, add loss, reset
+
+
+});
